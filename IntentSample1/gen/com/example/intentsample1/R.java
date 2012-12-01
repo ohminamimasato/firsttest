f6047a677e4afccfd72fc7f72c77f4d80e222fb7
ohminami.masato <ohminami.masato@sea.plala.or.jp>
2012-12-01 21:29:35 +0900

    drawableファイルに3つ追加

--- IntentSample1/gen/com/example/intentsample1/R.java
+++ IntentSample1/gen/com/example/intentsample1/R.java
@@ -14,8 +14,11 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int apple=0x7f020000;
+        public static final int banana=0x7f020001;
+        public static final int grape=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
         public static final int backbutton=0x7f080002;
