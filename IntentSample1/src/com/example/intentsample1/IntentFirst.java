3996803a3450c56e38cb19a2f121ab380801f597
ohminami.masato <ohminami.masato@sea.plala.or.jp>
2012-12-01 20:14:34 +0900

    IntentFirstクラスの作成

--- IntentSample1/src/com/example/intentsample1/IntentFirst.java
+++ IntentSample1/src/com/example/intentsample1/IntentFirst.java
@@ -1,8 +1,12 @@
 package com.example.intentsample1;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class IntentFirst extends Activity {
 
@@ -10,11 +14,32 @@ public class IntentFirst extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_first);
+
+
+        ListView listview =(ListView)findViewById(R.id.fruitlist);
+        listview.setOnItemClickListener(new ListItemClicklistener());
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_intent_first, menu);
-        return true;
+    class ListItemClickListener implements OnItemClickListener {
+
+     public void onItemClick(AdapterView<?> parent,
+		   View view,
+		   int position,
+		   long id) {
+
+    	 ListView listview =(ListView) parent;
+
+    	 String item =(String)listview.getItemAtPosition(position);
+
+    	 Intent intent = new Intent(IntentFirst.this,SecondActivity.class);
+
+    	 intent.putExtra("SELECTED_PICT", item);
+
+    	 startActivity(intent);
+
+
     }
+
+   }
 }
