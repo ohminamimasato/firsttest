0a373a9ed01a486864c92c63d8920af478bbc07d
ohminami.masato <ohminami.masato@sea.plala.or.jp>
2012-12-01 21:19:33 +0900

    SecondActivity完成、secondLayoutバグ修正

--- IntentSample1/gen/com/example/intentsample1/R.java
+++ IntentSample1/gen/com/example/intentsample1/R.java
@@ -18,9 +18,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int backbutton=0x7f080002;
         public static final int fruitimage=0x7f080001;
         public static final int fruitlist=0x7f080000;
-        public static final int menu_settings=0x7f080002;
+        public static final int menu_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_intent_first=0x7f030000;
--- IntentSample1/src/com/example/intentsample1/SecondActivity.java
+++ IntentSample1/src/com/example/intentsample1/SecondActivity.java
@@ -1,5 +1,46 @@
 package com.example.intentsample1;
 
-public class SecondActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
-}
+public class SecondActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondlayout);
+
+		Intent data=getIntent();
+
+		Bundle extras = data.getExtras();
+		String disp_pict=extras !=null ?extras.getString("SELECTED_PICT"):"";
+
+		ImageView image = (ImageView)findViewById(R.id.fruitimage);
+
+		if(disp_pict.equals("Apple")){
+			image.setImageResource(R.drawable.apple);
+		}
+		else if	(disp_pict.equals("banana")){
+			image.setImageResource(R.drawable.banana);
+		}
+		else if	(disp_pict.equals("Grape")){
+			image.setImageResource(R.drawable.grape);
+		}
+
+		Button button = (Button)findViewById(R.id.backbutton);
+
+		button.setOnClickListener(new ButtonClickListener());
+	}
+
+	class ButtonClickListener implements OnClickListener{
+
+		public void onClick(View v) {
+			finish();
+		}
+	}
+
+  }
